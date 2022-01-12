d9c8b6a85398b9495c29452430986e49e5df8cbd
Carlie Smith <carliesmith@amrock.com>
2022-01-12 15:57:49 -0500

    connected to api and retrieved data elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,48 @@
 var currentDayEl = document.querySelector("#city-deets")
+var startSearchEl = document.querySelector(".is-info");
+var cityInputEl = document.querySelector("#city");
+var cityContainer = document.getElementById('city-deets');
+
+var getCityName = function(event) {
+    //get the city name from the search box
+    event.preventDefault();
+    var cityName = cityInputEl.value.trim();
+    if(cityName) {
+        getCurrentDay(cityName);
+        cityInputEl ="";
+    } else {
+        alert("Please choose a city.");
+        }
+        console.log(event);
+    
+     }
 
 
 var getCurrentDay = function() {
-    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Detroit&appid=5796c450ae9a19c822066f04beb985b7";
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Detroit&appid=5796c450ae9a19c822066f04beb985b7&units=imperial";
     fetch(apiURL).then(function(response) {
         return response.json();
         })
     .then(function(data) {
-        console.log(data);
-        for (var i = 0; i <data.length; i++) {
-            console.log(data[i].main.temp)
-            console.log(data[i].temp)
-        }
-    })
+        console.log(data.main.temp, data.main.humidity);
+        //for (var i = 0; i <data.length; i++) {
+            var cityItems = document.createElement('ul');
+            var cityTemp = document.createElement('li');
+            cityTemp.textContent = "Temperature " + data.main.temp;
+            var cityHumidity = document.createElement('li');
+            cityHumidity.textContent = "Humidity " + data.main.humidity;
+          
+            cityContainer.appendChild(cityItems);
+            cityItems.appendChild(cityTemp);
+            cityItems.appendChild(cityHumidity);
+
+                  
+    });
+}
 //var displayCurrentDay = function()
 
 
 
-};
+startSearchEl.addEventListener("click", getCityName);
 
 getCurrentDay();
