ca336c27720e074e7f2ff43141d19ea92071724d
Carlie Smith <carliesmith@amrock.com>
2022-01-14 18:09:13 -0500

    got current and 5day forecasts working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,86 +2,95 @@
 var startSearchEl = document.getElementById("is-info");
 var cityInputEl = document.getElementById('city');
 var cityContainer = document.getElementById('city-deets');
+var day1Container = document.getElementById('days');
+var date = new Date();
 
+
+//get user input for city to search
 var getCityName = function(event) {
     //get the city name from the search box
     event.preventDefault();
-    var searchCity = cityInputEl.value.trim();
-    if(searchCity) {
-        getCurrentDay(searchCity);
-        console.log(searchCity);
-        getForecast(searchCity);
+    var searchCityST = cityInputEl.value.trim();
+    if(searchCityST) {
+        getLocation(searchCityST);
+         
      } else {
         alert("Please choose a city or provide just the name of the city.");
         }
 };
-     
-var getCurrentDay = function(name) {
-    var apiURL = "https://dataservice.accuweather.com/locations/v1/cities/search?apikey=UCqiTrpHfka3AP7MAaQAp5iVWw9ynNdp&q=" + name;
-    fetch(apiURL).then(function(response) {
+
+//pass the city,st into the location api
+var getLocation = function(name) {
+    var locationURL = "https://dataservice.accuweather.com/locations/v1/cities/search?apikey=UCqiTrpHfka3AP7MAaQAp5iVWw9ynNdp&q=" + name;
+    fetch(locationURL).then(function(response) {
         return response.json();
+    
         })
     .then(function(data) {
-       // Build the city details div
-            var cityItems = document.createElement('ul');
-            //cityItems.setAttribute("list-style-type", "disc");
-            var cityName = document.getElementById('cityHere');
-            cityName.textContent = data.list[0].name;
-            var cityConditions = document.createElement('li');
-            cityConditions.textContent = "Conditions: " + data.list[0].weather[0].description;
-            var cityTemp = document.createElement('li');
-            cityTemp.textContent = "Temperature: " + data.list[0].main.temp + " ºF";
-            var cityHumidity = document.createElement('li');
-            cityHumidity.textContent = "Humidity: " + data.list[0].main.humidity +"%";
-            var cityWind = document.createElement('li');
-            cityWind.textContent = "Wind speed: " + data.list[0].wind.speed + "MPH";
-            var cityUV = document.createElement('li');
-            cityUV.textContent = "UV index ";
-            
-            cityContainer.appendChild(cityItems);
-            cityItems.appendChild(cityName);
-            cityItems.appendChild(cityConditions);
-            cityItems.appendChild(cityTemp);
-            cityItems.appendChild(cityHumidity);
-            cityItems.appendChild(cityWind);
-           
-               
+        console.log(data[0].Key);
+        var cityKey = data[0].Key;
+        getCurrentForecast(cityKey);
+        get5dayForecast(cityKey);
     });
 }
 
-var getCityKey = function(name) {
-    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + name + "&appid=5796c450ae9a19c822066f04beb985b7&units=imperial";
-    fetch(forecastURL).then(function(response) {
-        return response.json();
-        })
-    .then(function(data) {
-        console.log(data);
-        //Get 5 days of data
-        var forecastItems = document.createElement('ul');
-            //cityItems.setAttribute("list-style-type", "disc");
-            var date = document.createElement('li');
-            date.textContent = data.list.dt;
+//pass the location key into the forecast api
+var getCurrentForecast = function(key) {
+            var forecastURL = "https://dataservice.accuweather.com/forecasts/v1/daily/5day/" + key + ".json?apikey=UCqiTrpHfka3AP7MAaQAp5iVWw9ynNdp";
+            fetch(forecastURL).then(function(response) {
+                return response.json();
+                })
+            .then(function(data) {
+       // Build the city details div
+            var cityItems = document.createElement('ul');
             var cityName = document.getElementById('cityHere');
-            cityName.textContent = data.list[0].name;
+            cityName.textContent = cityInputEl.textContent;
             var cityConditions = document.createElement('li');
-            cityConditions.textContent = "Conditions: " + data.list[0].weather[0].description;
-            var cityTemp = document.createElement('li');
-            cityTemp.textContent = "Temperature: " + data.list[0].main.temp + " ºF";
-            var cityHumidity = document.createElement('li');
-            cityHumidity.textContent = "Humidity: " + data.list[0].main.humidity +"%";
-            var cityWind = document.createElement('li');
-            cityWind.textContent = "Wind speed: " + data.list[0].wind.speed + "MPH";
-            var cityUV = document.createElement('li');
-            cityUV.textContent = "UV index ";
+            cityConditions.textContent = "Conditions: " + data.DailyForecasts[0].Day.IconPhrase;
+            var cityTempHigh = document.createElement('li');
+            cityTempHigh.textContent = "High temp for the day: " + data.DailyForecasts[0].Temperature.Maximum.Value + " ºF";
+            var cityTempLow = document.createElement('li');
+            cityTempLow.textContent = "Low temp for the day: " + data.DailyForecasts[0].Temperature.Minimum.Value + " ºF";
             
             cityContainer.appendChild(cityItems);
             cityItems.appendChild(cityName);
             cityItems.appendChild(cityConditions);
-            cityItems.appendChild(cityTemp);
-            cityItems.appendChild(cityHumidity);
-            cityItems.appendChild(cityWind);
-           
+            cityItems.appendChild(cityTempHigh);
+            cityItems.appendChild(cityTempLow);
+                        
     });
+}
+//grab the same url results and use in 5 day forecast
+var get5dayForecast = function(key) {
+        var forecastURL = "https://dataservice.accuweather.com/forecasts/v1/daily/5day/" + key + ".json?apikey=UCqiTrpHfka3AP7MAaQAp5iVWw9ynNdp";
+        fetch(forecastURL).then(function(response) {
+            return response.json();
+            })
+        .then(function(data) {
+            console.log(data.DailyForecasts);
+            // Build the 5 day forecast
+        
+            currentIndex = 0;
+          
+            for (i = 1; i < data.DailyForecasts.length; i++) {
+            var day1Items = document.createElement('div');
+            day1Items.setAttribute("class", "eachday");
+            var dayDate = document.createElement('p');
+            dayDate.textContent = "Date: " + data.DailyForecasts[i].Date;
+            var cityConditions = document.createElement('p');
+            cityConditions.textContent = "Conditions: " + data.DailyForecasts[i].Day.IconPhrase;
+            var cityTempHigh = document.createElement('p');
+            cityTempHigh.textContent = "High temp for the day: " + data.DailyForecasts[i].Temperature.Maximum.Value + " ºF";
+            var cityTempLow = document.createElement('p');
+            cityTempLow.textContent = "Low temp for the day: " + data.DailyForecasts[i].Temperature.Minimum.Value + " ºF";
+            
+            day1Container.appendChild(day1Items);
+            day1Items.appendChild(dayDate);
+            day1Items.appendChild(cityConditions);
+            day1Items.appendChild(cityTempHigh);
+            day1Items.appendChild(cityTempLow);
+    };
+});
 }
 
 
